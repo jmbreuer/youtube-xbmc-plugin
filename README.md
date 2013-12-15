d103c7aabb3ca5849b5c0e93ccd500ce1e181afc
Joachim Breuer <git@jmbreuer.net>
2013-12-15 15:40:40 +0100

    Prepare for debugging with PyDev/Eclipse

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ youtube-xbmc-plugin
 ===================
 
 Official Youtube Plugin for XBMC (Now on GitHub)
+
+
+http://wiki.xbmc.org/index.php?title=HOW-TO:Debug_Python_Scripts_with_Eclipse
--- plugin/default.py
+++ plugin/default.py
@@ -29,6 +29,21 @@ try:
 except ImportError:
     import xbmcvfsdummy as xbmcvfs
 
+REMOTE_DBG = False 
+
+# append pydev remote debugger
+if REMOTE_DBG:
+    # Make pydev debugger works for auto reload.
+    # Note pydevd module need to be copied in XBMC\system\python\Lib\PyDev
+    try:
+        import pysrc.pydevd as pydevd
+    # stdoutToServer and stderrToServer redirect stdout and stderr to eclipse console
+        pydevd.settrace('localhost', stdoutToServer=True, stderrToServer=True)
+    except ImportError:
+        sys.stderr.write("Error: " +
+            "You must add org.python.pydev.debug.pysrc to your PYTHONPATH.")
+        sys.exit(1)
+
 # plugin constants
 version = "3.4.6"
 plugin = "YouTube-" + version
